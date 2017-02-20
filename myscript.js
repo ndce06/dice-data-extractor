3fb2cb8dd38a1571e794cd2dc51faed94d8f31e5
Nirav Patel <nd_ce06@yahoo.co.in>
2017-02-20 20:01:09 +0530

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,6 +1,10 @@
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    if (request.action == "getDOM")
+    if (request.action == "getDOM") {
         sendResponse({ dom: document.getElementsByTagName('body')[0].innerHTML });
-    else
+    } else if(request.action == "goToNextPage") {
+    	$('#dice_paging_top').find('span.icon-filled-arrow-66').parent().click();
+    	sendResponse({ dom: document.getElementsByTagName('body')[0].innerHTML });
+    } else {
         sendResponse({}); // Send nothing..
+    }
 });
